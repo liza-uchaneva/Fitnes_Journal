6f7cfc40edebeb16527ddc6b43a48985a2be7cbf
lizau <liza.uchaneva@outlook.com>
2024-09-09 14:48:15 -0700

    create base for project

---
+++ Fitness_Journal/ClientApp/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import mkcert from 'vite-plugin-mkcert'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue(), mkcert()],
+    server: {
+        port: 3399,
+        https: true,
+        strictPort: true,
+        proxy: {
+            '/api': {
+                target: 'https://localhost:7153',
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/api/, '/api')
+            }
+        }
+    }
+})
---
+++ Fitness_Journal/Startup.cs
@@ -0,0 +1,38 @@
+﻿namespace Fitness_Journal
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; protected set; }
+        public Startup(IWebHostEnvironment env)
+        {
+            this.Environment = env;
+            var builder = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            //string conn = Configuration.GetConnectionString("ApplicationTable");
+            //services.AddDbContext<ApplicationsContext>(opt => opt.UseSqlServer(conn));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
+            });
+        }
+    }
+}
